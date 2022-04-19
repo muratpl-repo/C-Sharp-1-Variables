4b92cb33b8e4b5a96b432110273cd1445508aeb8
muratpala007 <muratpala007@gmail.com>
2022-04-19 15:48:46 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2Ders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char karakter = 'K'; //2 byte
+            string isim = "Kaan"; //8 byte
+            int tamSayi = 15; //4 byte
+            float kesirliSayi = 3.4f; //4 byte;
+            double buyukKesirliSayi = 4.5; //8 byte
+            bool anahtar = true; //1 byte
+
+            Console.WriteLine(sizeof(int));
+            Console.WriteLine(sizeof(char));
+            Console.WriteLine(sizeof(float));
+            Console.WriteLine(sizeof(bool));
+            Console.WriteLine(sizeof(double));
+            Console.WriteLine(isim.Length * sizeof(char));
+
+
+            Console.ReadLine();
+        }
+    }
+}
